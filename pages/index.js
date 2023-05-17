de296ed5835172908d96b135d29e6e11a98e9fef
andy <andy.yu617@gmail.com>
2023-05-17 23:21:42 +0000

    "Initial commit"

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,15 @@
 function Gallery({ images }) {
   return (
-    <div className="max-w-2xl mx-auto py-16 px-4 sm:py-24 sm:px-6 lg:max-w-7xl lg:px-8">
-      <div className="grid grid-cols-1 gap-y-10 sm:grid-cols-2 gap-x-6 lg:grid-cols-3 xl:grid-cols-4 xl:gap-x-8">
-        {images.map((image) => (
-          <img key={image.id} src={image.url} alt={`Image ${image.id}`} />
-        ))}
+    <>
+      <div>Hello World</div>
+      <div className="max-w-2xl mx-auto py-16 px-4 sm:py-24 sm:px-6 lg:max-w-7xl lg:px-8">
+        <div className="grid grid-cols-1 gap-y-10 sm:grid-cols-2 gap-x-6 lg:grid-cols-3 xl:grid-cols-4 xl:gap-x-8">
+          {images.map((image) => (
+            <img key={image.id} src={image.url} alt={`Image ${image.id}`} />
+          ))}
+        </div>
       </div>
-    </div>
+    </>
   );
 }
 
